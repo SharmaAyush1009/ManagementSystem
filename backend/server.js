48d26f92de1a75271fc71ad3f85013b2eb858079
SharmaAyush1009 <158248821+SharmaAyush1009@users.noreply.github.com>
2025-04-13 17:30:40 +0530

    Update server.js
    updated vercel link

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 // app.use(cors());
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://management-system-indol.vercel.app/login'],
+  origin: ['http://localhost:3000', 'https://management-system-indol.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Add all methods you use
   allowedHeaders: ['Content-Type', 'Authorization'] // Add custom headers
