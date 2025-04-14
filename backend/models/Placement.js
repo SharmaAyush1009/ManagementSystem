8708496dd5d8a0ff2428f7bdaaf2a8b152c0a9ca
SharmaAyush1009 <158248821+SharmaAyush1009@users.noreply.github.com>
2025-04-14 14:15:47 +0530

    removed unnecessary comments

--- backend/models/Placement.js
+++ backend/models/Placement.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const placementSchema = new mongoose.Schema({
   name: { type: String, required: true },
   batch: { type: Number, required: true },
-  branch: { type: String, required: true }, // This is the department
+  branch: { type: String, required: true }, 
   company: { type: String, required: true },
-  package: { type: Number, required: true }, // Store LPA
+  package: { type: Number, required: true }, 
   cpi: { type: Number, required: true },
   gender: { type: String, enum: ['Male', 'Female'], required: true },
   createdAt: { type: Date, default: Date.now }
