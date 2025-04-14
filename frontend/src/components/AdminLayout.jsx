01e7f89befb378f3f4b2e39936ecbfc689e13357
SharmaAyush1009 <158248821+SharmaAyush1009@users.noreply.github.com>
2025-04-14 14:32:11 +0530

    Update AdminLayout.jsx

--- frontend/src/components/AdminLayout.jsx
+++ frontend/src/components/AdminLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 const AdminLayout = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-      <Outlet /> {/* ✅ This will switch between AdminDashboard and ScoreCardPage */}
+      <Outlet /> {/* This will switch between AdminDashboard and ScoreCardPage */}
     </div>
   );
 };
