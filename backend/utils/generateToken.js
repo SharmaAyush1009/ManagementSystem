3ed5f9a9ba443a67634300a9a66d30c017153af9
SharmaAyush1009 <158248821+SharmaAyush1009@users.noreply.github.com>
2025-04-22 02:47:25 +0530

    expiry time to 1 in jwt

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,8 +1,8 @@
 const jwt = require("jsonwebtoken");
 
-const generateToken = (userId) => {
-  return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
-    expiresIn: "7d", // Token expires in 7 days
+const generateToken = (userId, role) => {
+  return jwt.sign({ id: userId, role }, process.env.JWT_SECRET, {
+    expiresIn: "1h", // expires in 1 hr
   });
 };
 
